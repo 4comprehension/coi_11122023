3d0b4d1769a72327b5675deade5ebff35278c784
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-13 16:48:02 +0100

    29. application/json vs x-ndjson vs sse

--- spring-webflux-demo/src/main/java/com/example/demo/ReactiveMovieController.java
+++ spring-webflux-demo/src/main/java/com/example/demo/ReactiveMovieController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import jdk.jfr.ContentType;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 class ReactiveMovieController {
@@ -13,8 +17,18 @@ class ReactiveMovieController {
         this.userService = userService;
     }
 
-    @GetMapping("/movies")
+    @GetMapping(value = "/movies-json", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<MovieService.MovieWithDescription> getMovies() {
-        return userService.getMovies();
+        return userService.getMovies().delayElements(Duration.ofSeconds(5));
+    }
+
+    @GetMapping(value = "/movies-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<MovieService.MovieWithDescription> getMovies2() {
+        return userService.getMovies().delayElements(Duration.ofSeconds(5));
+    }
+
+    @GetMapping(value = "/movies-ndjson", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MovieService.MovieWithDescription> getMovies3() {
+        return userService.getMovies().delayElements(Duration.ofSeconds(5));
     }
 }
