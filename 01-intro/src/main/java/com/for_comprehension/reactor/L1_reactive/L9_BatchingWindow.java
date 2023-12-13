8f484db46465f227196a28254a7166d904900db4
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-13 09:27:21 +0100

    18. Buffer/Window - examples

---
+++ 01-intro/src/main/java/com/for_comprehension/reactor/L1_reactive/L9_BatchingWindow.java
@@ -0,0 +1,26 @@
+package com.for_comprehension.reactor.L1_reactive;
+
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+class L9_BatchingWindow {
+    public static void main(String[] args) {
+        Flux<Long> ticker = Flux.interval(Duration.ofMillis(200));
+
+        ticker
+          .window(Duration.ofSeconds(2), Duration.ofSeconds(3))
+          .flatMap(Flux::collectList)
+          .doOnNext(System.out::println)
+          .blockLast();
+    }
+
+    public static void buffering(String[] args) {
+        Flux<Long> ticker = Flux.interval(Duration.ofMillis(200));
+
+        ticker
+          .buffer(Duration.ofSeconds(1), Duration.ofSeconds(2))
+          .doOnNext(System.out::println)
+          .blockLast();
+    }
+}
