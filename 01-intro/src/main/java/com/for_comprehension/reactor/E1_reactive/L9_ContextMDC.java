a7cc1f769b54dc8f2a0f1eff560cda093c7df122
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-13 12:51:42 +0100

    25. Context propagation tooling

--- 01-intro/src/main/java/com/for_comprehension/reactor/E1_reactive/L9_ContextMDC.java
+++ 01-intro/src/main/java/com/for_comprehension/reactor/E1_reactive/L9_ContextMDC.java
@@ -8,6 +8,8 @@ import reactor.core.publisher.Mono;
 import reactor.core.publisher.Signal;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.Loggers;
+import reactor.util.context.Context;
+import reactor.util.context.ContextView;
 
 import java.time.Duration;
 import java.util.function.Consumer;
@@ -23,28 +25,70 @@ class L9_ContextMDC {
 
         Flux.interval(Duration.ofSeconds(1))
           .take(5)
-          .map(withMdc(i -> {
+          .flatMap(withMdc(i -> {
               log.info("map: " + i);
               return i;
           }))
+          .flatMap(withThreadLocalValue(i -> {
+              log.info("map: " + i);
+              return i;
+          }, ctx -> MDC.put("tid", ctx.get("tid")), ctx -> MDC.remove("tid")))
           .publishOn(Schedulers.boundedElastic())
           .flatMap(i -> Flux.just(i).subscribeOn(Schedulers.boundedElastic()))
           .filter(i -> true)
           .doOnEach(mdc(i -> log.info(i.toString())))
-          .log()
+          .doOnEach(withThreadLocalValue(i -> {
+              log.info("doOnEach: " + i);
+          }, ctx -> MDC.put("tid", ctx.get("tid")), ctx -> MDC.remove("tid")))
           .map(i -> i)
+          .contextWrite(Context.of("tid", MDC.get("tid")))
           .blockLast();
-
     }
 
     // recover "tid" value and put it in MDC, cleanup after
     private static <T, R> Function<T, Mono<R>> withMdc(Function<T, R> function) {
-        return null; // TODO
+        return t -> Mono.deferContextual(ctx -> Mono.fromCallable(() -> {
+            try (var __ = MDC.putCloseable("tid", ctx.get("tid"))) {
+                return function.apply(t);
+            }
+        }));
+    }
+
+    private static <T, R> Function<T, Mono<R>> withThreadLocalValue(Function<T, R> function,
+                                                                    Consumer<ContextView> setter,
+                                                                    Consumer<ContextView> cleanup) {
+        return t -> Mono.deferContextual(ctx -> Mono.fromCallable(() -> {
+            setter.accept(ctx);
+            try {
+                return function.apply(t);
+            } finally {
+                cleanup.accept(ctx);
+            }
+        }));
+    }
+
+    private static <T> Consumer<Signal<T>> withThreadLocalValue(Consumer<T> consumer,
+                                                                Consumer<ContextView> setter,
+                                                                Consumer<ContextView> cleanup) {
+        return signal -> {
+            if (signal.isOnNext()) {
+                setter.accept(signal.getContextView());
+                try {
+                    consumer.accept(signal.get());
+                } finally {
+                    cleanup.accept(signal.getContextView());
+                }
+            }
+        };
     }
 
     private static <T> Consumer<Signal<T>> mdc(Consumer<T> consumer) {
-        return null; // TODO
+        return signal -> {
+            if (signal.isOnNext()) {
+                try (var __ = MDC.putCloseable("tid", signal.getContextView().get("tid"))) {
+                    consumer.accept(signal.get());
+                }
+            }
+        };
     }
-
-
 }
