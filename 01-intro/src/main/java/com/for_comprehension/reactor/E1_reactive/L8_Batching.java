8ba7736f106323398a647ab0660c4111620dc944
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-12-13 10:16:33 +0100

    18. Buffer/Window - solutions

--- 01-intro/src/main/java/com/for_comprehension/reactor/E1_reactive/L8_Batching.java
+++ 01-intro/src/main/java/com/for_comprehension/reactor/E1_reactive/L8_Batching.java
@@ -12,14 +12,22 @@ class L8_Batching {
      * Buffer the incoming {@link Flux} into lists of N items and then emit these lists
      */
     static Flux<List<Integer>> L1_buffer(Flux<Integer> numbers, int n) {
-        return WorkshopUtils.todo();
+        return numbers.buffer(n);
+    }
+
+    static Flux<List<Integer>> L1_buffer2(Flux<Integer> numbers, int n) {
+        return numbers.window(n).flatMap(Flux::collectList);
     }
 
     /**
      * Buffer the incoming {@link Flux} into lists of max N items... but do not wait more than provided timeout
      */
     static Flux<List<Integer>> L2_bufferOrTimeout(Flux<Integer> numbers, int n, Duration timeout) {
-        return WorkshopUtils.todo();
+        return numbers.bufferTimeout(n, timeout);
+    }
+
+    static Flux<List<Integer>> L2_bufferOrTimeout2(Flux<Integer> numbers, int n, Duration timeout) {
+        return numbers.windowTimeout(n, timeout).flatMap(Flux::collectList);
     }
 
     /**
@@ -34,6 +42,10 @@ class L8_Batching {
      * Bonus: buffer into LinkedLists
      */
     static Flux<List<Integer>> L3_overlappingBuffer(Flux<Integer> numbers, int maxSize, int skip) {
-        return WorkshopUtils.todo();
+        return numbers.buffer(maxSize, skip);
+    }
+
+    static Flux<List<Integer>> L3_overlappingBuffer2(Flux<Integer> numbers, int maxSize, int skip) {
+        return numbers.window(maxSize, skip).flatMap(Flux::collectList);
     }
 }
